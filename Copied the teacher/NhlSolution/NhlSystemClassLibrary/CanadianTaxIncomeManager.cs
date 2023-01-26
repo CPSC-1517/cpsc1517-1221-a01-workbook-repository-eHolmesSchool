b9440cb4bbf9663fa1db3773a5a25c4a0a703018
EvanHolmes <squippysquerp@gmail.com>
2023-01-26 11:51:20 -0700

    Did some more I guess

---
+++ Copied the teacher/NhlSolution/NhlSystemClassLibrary/CanadianTaxIncomeManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    public class CanadianTaxIncomeManager
+    {
+
+        public List<CanadianIncomeTaxData> ConvertToCanadianIncomeTax(List<string> lines)
+        {
+            List<CanadianIncomeTaxData> dataList = new List<CanadianIncomeTaxData>();
+            //Iterate through each element in the list
+            foreach (string currentLine in lines)
+            {
+                string[] tokens = currentLine.Split(',');
+                CanadianIncomeTaxData data = new CanadianIncomeTaxData();
+                data.RegionAbbreviation = tokens[0];
+                data.RegionName = tokens[1];
+                data.TaxYear = int.Parse(tokens[2]);
+                data.StartingIncome = decimal.Parse( tokens[4]);
+                data.EndingIncome = decimal.Parse(tokens[5]);
+                data.TaxRate = double.Parse(tokens[6]);
+                data.BaseTaxAmount = decimal.Parse(tokens[7]);
+
+                dataList.Add(data);
+
+            }
+            return dataList;
+        }
+
+        public List<string> LoadFromCsvFile(string csvFilePath)
+        {
+            List<string> allLines = new List<string>();
+            //using (StreamReader reader = new StreamReader(csvFilePath))
+            //{
+            //    string currentLine;
+            //    // Read and display lines from the file until the end of
+            //    // the file is reached.
+            //    while ((currentLine = reader.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(currentLine);
+            //    }
+
+            //}
+
+            string[] lineArray = File.ReadAllLines(csvFilePath);
+
+            //foreach (string currentLine in lineArray)
+            //{
+            //    allLines.Add(currentLine);
+            //}
+
+            for (int index = 1; index < lineArray.Length; index++)
+            {
+                allLines.Add((string)lineArray[index]);
+            }
+
+            return allLines;
+        }
+    }
+}
---
+++ Copied the teacher/NhlSolution/NhlSystemTestProject/CanadianTaxIncomeTest.cs
@@ -0,0 +1,41 @@
+﻿using NhlSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemTestProject
+{
+    [TestClass]
+    public class CanadianTaxIncomeTest
+    {
+        [TestMethod]
+        [DataRow(@"..\..\..\Data\CanadianPersonalIncomeTaxRates.csv", 439)]
+
+        public void LoadFromCsvFile_RowCount_CorrectCount(string csvFilePath, int expectedCount)//highlight this, right click, click Run Tests
+        {
+            var dataManager = new CanadianTaxIncomeManager();
+            List<string> allLinesFromFile = dataManager.LoadFromCsvFile(csvFilePath);
+
+            Assert.AreEqual(439, allLinesFromFile.Count); //We simply open the file from the downloads folder and count the lines. 
+        }
+
+        [TestMethod]
+        [DataRow(@"..\..\..\Data\CanadianPersonalIncomeTaxRates.csv", 439)]
+        public void ConvertToCanadianIncomeTax_CorrectConversion_ExpectedResults(string csvFilePath, int expectedCount)
+        {
+            var dataManager = new CanadianTaxIncomeManager();
+            List<string> allLinesFromFile = dataManager.LoadFromCsvFile(csvFilePath);
+
+            List<CanadianIncomeTaxData> incomeTaxData = dataManager.ConvertToCanadianIncomeTax(allLinesFromFile);
+
+            Assert.AreEqual(expectedCount, allLinesFromFile.Count); //We simply open the file from the downloads folder and count the lines. 
+
+            int firstIndex = 0;
+            int lastIndex = incomeTaxData.Count - 1;
+
+            Assert.AreEqual("CAN", incomeTaxData[lastIndex], RegionAbbreviation);
+        }
+    }
+}
